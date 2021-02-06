c07c16c950bc04accbed9970086fc9ae78db00b5
Camille Gutierrez <cmg74769@usc.edu>
2021-02-06 12:28:17 -0800

    testing

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -32,7 +32,7 @@ from grovepi import *
 is, if you run `python3 grovepi_sensors.py` in terminal, this if-statement will 
 be true"""
 if __name__ == '__main__':
-    PORT = 4  #D4
+	PORT = 4  #D4
 
 	ultrasonic_ranger = 3 #Grove Ultrasonic ranger connected to digital port D3
 	potentiometer = 1 #Grove rotary angle sensor connected to analog port A1
@@ -51,19 +51,19 @@ if __name__ == '__main__':
 	full_angle = 300
 
 
-    while True:
+	while True:
 	try:
 
 		# Read sensor value from potentiometer
-        sensor_value = grovepi.analogRead(potentiometer)
+		sensor_value = grovepi.analogRead(potentiometer)
 
-        # Calculate voltage
-        voltage = round((float)(sensor_value) * adc_ref / 1023, 2)
+		# Calculate voltage
+		voltage = round((float)(sensor_value) * adc_ref / 1023, 2)
 
-        # Calculate rotation in degrees (0 to 300)
-        degrees = round((voltage * full_angle) / grove_vcc, 2)
+		# Calculate rotation in degrees (0 to 300)
+		degrees = round((voltage * full_angle) / grove_vcc, 2)
 
-        threshhold = (degrees/full_angle) * 517
+		threshhold = (degrees/full_angle) * 517
 
 		setText_norefresh(threshhold, 'cm \n')
 
@@ -77,8 +77,8 @@ if __name__ == '__main__':
 			setText_nofresh(threshhold, '            \n' )
 			setText_norefresh(distant, 'cm')
 
-        #So we do not poll the sensors too quickly which may introduce noise,
-        #sleep for a reasonable time of 200ms between each iteration.
-        time.sleep(0.2)
+		#So we do not poll the sensors too quickly which may introduce noise,
+		#sleep for a reasonable time of 200ms between each iteration.
+		time.sleep(0.2)
 
-        print(grovepi.ultrasonicRead(PORT))
+		print(grovepi.ultrasonicRead(PORT))
